1a4708dc234f669ec2260ff9b8cf10e386d8f1fd
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2022-11-06 11:09:39 -0300

    Responsive video

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -4,7 +4,7 @@ import video1 from "../../assets/videos/video.mp4";
 export function Video() {
   return (
     <VideoContainer>
-      <video controls>
+      <video width="100%" height="auto" controls>
         <source src={video1} type="video/mp4" />
       </video>
     </VideoContainer>
